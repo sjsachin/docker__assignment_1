22fc32dc50c8f5185febf999416f2958a12f2968
sjsachin <38081862+sjsachin@users.noreply.github.com>
2018-05-25 16:51:05 +0530

    Update output.py

--- output.py
+++ output.py
@@ -5,7 +5,7 @@ if model_1_score>model_2_score:
    print("model_1 is better ")
    print("model_1 score is")
    print(model_1_score)
-elif model_1_score < model_2_score::
+elif model_1_score < model_2_score:
     print("model_2 is better ")
     print("model_2 score is")
     print(model_2_score)

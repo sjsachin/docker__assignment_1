0095e9f9619496b136677d13a613e0221f154a59
sjsachin <38081862+sjsachin@users.noreply.github.com>
2018-05-25 17:41:10 +0530

    Update exploratory.py

--- exploratory.py
+++ exploratory.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-data1=pd.read_csv("C:\\Users\Sachin Jain\Desktop\dcker_test\Ex03_SystolicBP_Regreesion.csv")
+data1=pd.read_csv("Ex03_SystolicBP_Regreesion.csv")
 from sklearn.model_selection import train_test_split
 X=data1.iloc[:,0:-1]
 Y=data1.iloc[:,-1]

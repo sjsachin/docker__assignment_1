62d04a3010fb827f119313379d6911cf72b09111
sjsachin <38081862+sjsachin@users.noreply.github.com>
2018-05-25 17:36:17 +0530

    Update model_1.py

--- model_1.py
+++ model_1.py
@@ -1,5 +1,6 @@
 from sklearn.linear_model import LinearRegression
-from sklearn.linear_model import LinearRegression
+from exploratory.py import x_train,x_test,y_train,y_test
+from sklearn.metrics import r2_score
 linreg=LinearRegression()
 model_1=linreg.fit(x_train,y_train)
 predict_values_1=model_1.predict(x_test)

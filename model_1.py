521a083de0e05d573c1ec169f217a9d936eb64d9
sjsachin <38081862+sjsachin@users.noreply.github.com>
2018-05-25 17:44:42 +0530

    Update model_1.py

--- model_1.py
+++ model_1.py
@@ -4,4 +4,4 @@ from sklearn.metrics import r2_score
 linreg=LinearRegression()
 model_1=linreg.fit(x_train,y_train)
 predict_values_1=model_1.predict(x_test)
-model_1_score=r2_score(y_test,predicted_values_1)
+model_1_score=r2_score(y_test,predict_values_1)

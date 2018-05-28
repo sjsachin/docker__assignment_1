9f730e4b9d49eedc5a0bd896afee4cd7f5e7583a
sjsachin <38081862+sjsachin@users.noreply.github.com>
2018-05-28 17:47:21 +0530

    Update output.py

--- output.py
+++ output.py
@@ -4,10 +4,10 @@ from model_2 import *
 if model_1_score>model_2_score:
    print("model_1 is better ")
    print("model_1 score is")
-   print(model_1_score)
+   #print(model_1_score)
 elif model_1_score < model_2_score:
     print("model_2 is better ")
     print("model_2 score is")
-    print(model_2_score)
+    #print(model_2_score)
 else:
     print("Both model have equal accuracy")

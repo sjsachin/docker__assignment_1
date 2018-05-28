498a1077d1642d3710364c15bdbe3e811450a72a
sjsachin <38081862+sjsachin@users.noreply.github.com>
2018-05-28 13:04:50 +0530

    Add files via upload

---
+++ docker_shell.sh
@@ -0,0 +1,4 @@
+CMD cd /docker__assignment_1/ python exploratory.py
+CMD cd /docker__assignment_1/ python model_1.py
+CMD cd /docker__assignment_1/ python model_2.py
+CMD cd /docker__assignment_1/ python output.py
